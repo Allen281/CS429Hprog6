c6c61a9ed3ba79fd7c1f0ae6c646a37128cacb3a
Allen281 <allenli2835@gmail.com>
2026-02-24 17:58:11 -0600

    updated stats list

--- libtdmm/tdmm.c
+++ libtdmm/tdmm.c
@@ -8,9 +8,12 @@
 static header* headers_start;
 static header *headers_end;
 static size_t requested_size;
+static double average_utilization;
+static int operation_count;
 static size_t total_size;
 static alloc_strat_e strategy;
 static long page_size;
+static size_t data_structure_overhead;
 
 static void set_block_state(header* block, bool is_free, bool is_marked, size_t size, header* next) {
     block->is_free = is_free;
@@ -55,6 +58,8 @@ void t_init(alloc_strat_e strat) {
     headers_end = initial_block;
     requested_size = 0;
     total_size = page_size;
+    data_structure_overhead = sizeof(header);
+    average_utilization = 0;
 }
 
 void *t_malloc(size_t size) {
@@ -96,6 +101,9 @@ void *t_malloc(size_t size) {
     
     block->is_free = false;
     requested_size += block->size;
+    average_utilization += (double)requested_size / total_size;
+    data_structure_overhead += sizeof(header);
+    operation_count++;
     return (char*)block + sizeof(header);
 }
 
@@ -108,6 +116,7 @@ static void merge_blocks(header* block){
     
     block->size += sizeof(header) + block->next->size;
     block->next = block->next->next;
+    data_structure_overhead -= sizeof(header);
 }
 
 void t_free(void *ptr) {
@@ -140,14 +149,16 @@ void t_free(void *ptr) {
     header* block = (header*)((char*)ptr - sizeof(header));
     block->is_free = true;
     requested_size -= block->size;
+    average_utilization += (double)requested_size / total_size;
+    operation_count++;
     
     merge_blocks(block);
     merge_blocks(prev);
 }
 
 void t_display_stats() {
-    printf("Page size: %zu\n", page_size);
-    printf("Requested size: %zu\n", requested_size);
-    printf("Total size: %zu\n", total_size);
-    printf("%% memory utilization: %.2f%%\n", (double)requested_size / total_size * 100);
+    printf("Page size: %zu bytes\n", page_size);
+    printf("Total bytes requested from sys: %zu bytes\n", total_size);
+    printf("Data structure overhead: %zu bytes (%.2f%%)\n", data_structure_overhead, (double)data_structure_overhead / total_size * 100);
+    printf("Average %% memory utilization: %.2f%%\n", average_utilization/operation_count * 100);
 }
--- src/main.c
+++ src/main.c
@@ -18,22 +18,29 @@ int main(){
     printf("Size (Bytes) | t_malloc (ns) | t_free (ns)\n");
     printf("-------------------------------------------\n");
     
+    double average_malloc_time = 0.0;
+    double average_free_time = 0.0;
     for(size_t size = 1; size <= 1024 * 8; size++){
         clock_gettime(CLOCK_MONOTONIC, &start);
         void* ptr = t_malloc(size);
         clock_gettime(CLOCK_MONOTONIC, &end);
         
         double malloc_time = get_elasped_time(start, end);
+        average_malloc_time += malloc_time;
         
         clock_gettime(CLOCK_MONOTONIC, &start);
         t_free(ptr);
         clock_gettime(CLOCK_MONOTONIC, &end);
         
         double free_time = get_elasped_time(start, end);
+        average_free_time += free_time;
         
         printf("%12zu | %13.0f | %11.0f\n", size, malloc_time, free_time);
     }
+    printf("-------------------------------------------\n");
     
     t_display_stats();
+    printf("Average t_malloc time: %.0f ns\n", average_malloc_time / 8192);
+    printf("Average t_free time: %.0f ns\n", average_free_time / 8192);
     return 0;
 }
